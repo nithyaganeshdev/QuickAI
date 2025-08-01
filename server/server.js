4e03e3e7b64df2156ec7217eac207f3d93ca9f08
nithyaganeshdev <nithyaganeshm@gmail.com>
2025-08-01 13:52:45 +0530

    Implement clerk user auth middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 // Express Instance
 const app = express();
@@ -8,9 +9,12 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // Basic Home Route
 app.get("/", (req, res) => res.send("Server is Live! ✅"));
+
+app.use(requireAuth());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on the port ${3000} ✅`));
